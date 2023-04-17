a6c6f060402960053e122861709b3c240134991b
Fyan E. Widyantoro <fyan.widyantoro@bankmega.com>
2023-04-17 09:42:37 +0700

    Update pm2 ecosystem config prod

--- extras/pm2-nextdashboard.config.js
+++ extras/pm2-nextdashboard.config.js
@@ -1,33 +1,34 @@
 module.exports = {
-	apps: [
-		{
-			name: 'NextJSPG-DASHBOARD',
-			// instance: 2,
-			// cwd: '/Applications/Data/ReactNextJS/nextjs-testapp-argon',
-			// interpreter: '/Applications/Data/ReactNextJS/nextjs-testapp-argon/node_modules/.bin/node',
-			cwd: '/var/www/monitoring/nextjspg-dashboard/',
-			interpreter: '/var/www/monitoring/nextjspg-dashboard/node_modules/.bin/node',
-			script: 'node_modules/.bin/next',
-			args: 'start',
-			autorestart: true,
-			// max_memory_restart: '1G',
-			watch: true,
-			merge_log: true,
-			env: {
-				NODE_ENV: 'development',
-				API_URL: 'http://10.14.20.49',
-				PORT: 4000,
-			},
-			env_sandbox: {
-				NODE_ENV: 'sandbox',
-				API_URL: 'http://10.14.21.37',
-				PORT: 4000,
-			},
-			env_production: {
-				NODE_ENV: 'production',
-				API_URL: 'http://10.14.19.206',
-				PORT: 4000,
-			},
-		},
-	],
+  apps: [
+    {
+      name: 'NextJSPG-DASHBOARD',
+      // instance: 2,
+      // cwd: '/Applications/Data/ReactNextJS/nextjs-testapp-argon',
+      // interpreter: '/Applications/Data/ReactNextJS/nextjs-testapp-argon/node_modules/.bin/node',
+      cwd: '/var/www/monitoring/nextjspg-dashboard/',
+      interpreter:
+        '/var/www/monitoring/nextjspg-dashboard/node_modules/.bin/node',
+      script: 'node_modules/.bin/next',
+      args: 'start',
+      autorestart: true,
+      // max_memory_restart: '1G',
+      watch: true,
+      merge_log: true,
+      env: {
+        NODE_ENV: 'development',
+        API_URL: 'http://10.14.20.49',
+        PORT: 4000,
+      },
+      env_sandbox: {
+        NODE_ENV: 'sandbox',
+        API_URL: 'http://10.14.21.37',
+        PORT: 4000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        API_URL: 'http://monitorcommerce.bankmega.com',
+        PORT: 4000,
+      },
+    },
+  ],
 };
